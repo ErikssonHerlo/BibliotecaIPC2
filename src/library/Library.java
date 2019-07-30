0375d10b1c56d6d54282114514146be51cab1732
Eriksson Hernandez <erikssonhernandez201830459@cunoc.edu.gt>
2019-07-29 20:30:16 -0600

    Nueva Rama

--- src/library/Library.java
+++ src/library/Library.java
@@ -26,6 +26,23 @@ public class Library {
         BufferedReader br = null;
         Inicio Iniciar = new Inicio();
         Iniciar.setVisible(true);
+   /*     try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(NuevoTexto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(NuevoTexto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(NuevoTexto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NuevoTexto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        */
           
         // TODO code application logic here
     }
